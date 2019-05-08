741fdb5ec7a20e969a6668d1f6281a4412b0acb3
Niels Tanis <ntanis@veracode.com>
2019-05-07 21:00:40 -0700

    fix two build scripts

--- Demo/AssemblyLoadContext/MyPDFLibrary/build+deploy-debug.sh
+++ Demo/AssemblyLoadContext/MyPDFLibrary/build+deploy-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf ../FavoritePlaces/bin/Debug/netcoreapp2.2/plugins/MyPDFLibrary/
-dotnet publish -c Debug -r osx-x64 -o ../FavoritePlaces/bin/Debug/netcoreapp2.2/plugins/MyPDFLibrary --self-contained
+rm -rf ../FavoritePlaces/bin/Debug/netcoreapp2.2/MyPDFLibrary/
+dotnet publish -c Debug -r osx-x64 -o ../FavoritePlaces/bin/Debug/netcoreapp2.2/MyPDFLibrary --self-contained
